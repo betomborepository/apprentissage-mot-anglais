9520b0662c7c8c388a44d9e6d79b1483452947bd
Unknown <matiasy.rasolofoarimanana@gmail.com>
2018-05-08 01:12:56 +0400

    Auto stash before merge of "master" and "origin/master"
    Fake Update Test

---
+++ projet NetBeans/projetITUD/build/generated-sources/ap-source-output/entity/Test_.java
@@ -0,0 +1,17 @@
+package entity;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-06T19:52:54")
+@StaticMetamodel(Test.class)
+public class Test_ { 
+
+    public static volatile SingularAttribute<Test, Date> dateCreation;
+    public static volatile SingularAttribute<Test, Date> dateModification;
+    public static volatile SingularAttribute<Test, String> titre;
+    public static volatile SingularAttribute<Test, Integer> id;
+
+}
